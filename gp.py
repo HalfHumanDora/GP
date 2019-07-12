47c7a1e65b6045f0c70c2e1b42ccf7023c20f612
DaikiTanak <daiki.yosky@gmail.com>
2019-07-12 23:51:19 +0900

    main function

---
+++ gp.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+
+class Gaussian_Process(object):
+    def __init__(self):
+        pass
+
+
+    def fit(self, X, y):
+        pass
+
+
+    def predict(self, X):
+        pass
